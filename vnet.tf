8017a8cbb1feb096779c03037ee12fbf6fa974e3
Elena-Ladyjenskaia <Elena.Ladyjenskaia@dpdhl.com>
2021-04-06 19:04:21 +0000

    Added vnet module

---
+++ vnet.tf
@@ -0,0 +1,9 @@
+
+
+module "network" {
+  source  = "app.terraform.io/ELA1-training/network/azurerm"
+  version = "3.0.1"
+  # insert required variables here
+  resource_group_name = var.resource_group_name
+}
+
